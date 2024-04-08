8b73db5cdb968268d252c54c13504d75532d0af3
lisa <elizabet.liber@gmail.com>
2024-04-09 02:06:50 +0500

    last changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500 p-5 text-white text-center">
-      <div className="flex flex-col items-center max-w-xl drop-shadow-lg gap-y-4">
-        <h1 className="text-3xl">Последние новости группы ЭСб-23-2</h1>
-        <p className="">Недавно некоторые студенты были замешаны в терроризме. Среди них и студенты нашей группы. Особенно обратите внимание на опасного преступника <span className="text-[red]">Никиту Кика</span>. Он известен тем, что любит заёбывать людей и высасывать энергию. Рядом с ним сидеть не стоит. Вампиров в вузе не поощряем. Остальные - соучастники, но не менее опасные. <br /> <span className="underline decoration-2 text-xl">Будьте осторожны!</span></p>
-          <Image
-            src="/Однокурсники.png"
-            alt=""
-            width="100" height="100" layout="responsive" objectFit="contain"
-          />
+    <main className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen px-5 py-12 text-white text-center">
+      <div className="container mx-auto">
+        <div className="flex justify-between text-xl">
+        <Link href={""} className="tracking-wide hover:scale-125 ease-in duration-300 block px-14 py-3 bg-black rounded-full shadow-lg shadow-violet-900/100">О нас</Link>
+        <Link href={""} className="hover:scale-125 ease-in duration-300 block px-14 py-3 bg-black rounded-full shadow-lg shadow-violet-900/100">Новости</Link>
+        <Link href={""} className="hover:scale-125 ease-in duration-300 block px-14 py-3">ЭСб-23-2</Link>
+        <Link href={""} className="hover:scale-125 ease-in duration-300 block px-14 py-3 bg-black rounded-full shadow-lg shadow-violet-900/100">ДЗ</Link>
+        <Link href={""} className="hover:scale-125 ease-in duration-300 block px-14 py-3 bg-black rounded-full shadow-lg shadow-violet-900/100">Наши достижения</Link>
+      </div>
       </div>
     </main>
   );
